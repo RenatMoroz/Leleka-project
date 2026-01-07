48fffc253afaa5eb41341993a3dcdcd5c788fa4e
ken4ik77 <ken4ik77@gmail.com>
2026-01-07 20:51:41 +0200

    1 change

---
+++ components/layout/header/Header.tsx
@@ -0,0 +1,20 @@
+import css from './Header.module.css'
+import Link from 'next/link'
+import { AuthNavigation } from '../AuthNavigation/AuthNavigation'
+
+const Header = () => (<header className={css.header}>
+    
+    <Link href="/" aria-label="Home">
+       
+        <svg width="106" height="30" class="header-logo">
+          <use href=""></use>
+        </svg>
+         Лелека
+    </Link>
+                <Link href="/">
+                <svg width="106" height="30" class="header-logo">
+          <use href=""></use>
+        </svg></Link>
+</header>)
+
+export default Header
