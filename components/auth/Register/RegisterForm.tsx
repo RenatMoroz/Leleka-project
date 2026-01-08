637c5884d6bb3b88103b902d2bcba208f6c98572
Renat Moroz <bs4123555@gmail.com>
2026-01-08 13:26:23 +0100

    add front-sign-up-2

--- components/auth/Register/RegisterForm.tsx
+++ components/auth/Register/RegisterForm.tsx
@@ -19,80 +19,96 @@ export default function RegisterForm() {
 
   return (
     <section className={styles.wrapper}>
-      <div className={styles.logoContainer}>
-        <div className={styles.logoWrapper}>
-          <div className={styles.logo}>
-            <span className={styles.logoIcon}></span>
-            <span className={styles.logoText}>Лелека</span>
+      <div className={styles.containerRegister}>
+        <div className={styles.containerTwo}>
+          <div className={styles.logoContainer}>
+            <div className={styles.logoWrapper}>
+              <div className={styles.logo}>
+                <svg width="31" height="30" className="icon-leleka">
+                  <use href="/logo-icon.svg#icon-icon-leleka"></use>
+                </svg>
+                <svg width="61" height="13" className="text-leleka">
+                  <use href="/logo-icon.svg#icon-text-leleka"></use>
+                </svg>
+              </div>
+            </div>
+          </div>
+
+          <div className={styles.formCont}>
+            <h1 className={styles.title}>Реєстрація</h1>
+            <Formik
+              initialValues={{ name: '', email: '', password: '' }}
+              validationSchema={validationSchema}
+              onSubmit={() => {
+                router.push('/profile/edit');
+              }}
+            >
+              {({ isSubmitting }) => (
+                <Form className={styles.form}>
+                  <label className={styles.label}>
+                    Ім’я*
+                    <Field
+                      name="name"
+                      placeholder="Ваше імʼя"
+                      className={styles.input}
+                    />
+                    <ErrorMessage
+                      name="name"
+                      component="span"
+                      className={styles.error}
+                    />
+                  </label>
+
+                  <label className={styles.label}>
+                    Пошта*
+                    <Field
+                      name="email"
+                      placeholder="hello@leleka.com"
+                      className={styles.input}
+                    />
+                    <ErrorMessage
+                      name="email"
+                      component="span"
+                      className={styles.error}
+                    />
+                  </label>
+
+                  <label className={styles.label}>
+                    Пароль*
+                    <Field
+                      name="password"
+                      type="password"
+                      placeholder="********"
+                      className={styles.input}
+                    />
+                    <ErrorMessage
+                      name="password"
+                      component="span"
+                      className={styles.error}
+                    />
+                  </label>
+
+                  <button
+                    type="submit"
+                    className={styles.button}
+                    disabled={isSubmitting}
+                  >
+                    {isSubmitting ? 'Завантаження...' : 'Зареєструватися'}
+                  </button>
+
+                  <p className={styles.loginPrompt}>
+                    Вже маєте аккаунт?{' '}
+                    <Link href="/auth/login" className={styles.loginLink}>
+                      Увійти
+                    </Link>
+                  </p>
+                </Form>
+              )}
+            </Formik>
           </div>
         </div>
+        <div className={styles.background}></div>
       </div>
-
-      <h1 className={styles.title}>Реєстрація</h1>
-
-      <Formik
-        initialValues={{ name: '', email: '', password: '' }}
-        validationSchema={validationSchema}
-        onSubmit={() => {
-          router.push('/profile/edit');
-        }}
-      >
-        <Form className={styles.form}>
-          <label className={styles.label}>
-            Ім’я*
-            <Field
-              name="name"
-              placeholder="Ваше імʼя"
-              className={styles.input}
-            />
-            <ErrorMessage
-              name="name"
-              component="span"
-              className={styles.error}
-            />
-          </label>
-
-          <label className={styles.label}>
-            Пошта*
-            <Field
-              name="email"
-              placeholder="hello@leleka.com"
-              className={styles.input}
-            />
-            <ErrorMessage
-              name="email"
-              component="span"
-              className={styles.error}
-            />
-          </label>
-
-          <label className={styles.label}>
-            Пароль*
-            <Field
-              name="password"
-              type="password"
-              placeholder="********"
-              className={styles.input}
-            />
-            <ErrorMessage
-              name="password"
-              component="span"
-              className={styles.error}
-            />
-          </label>
-
-          <button type="submit" className={styles.button}>
-            Зареєструватися
-          </button>
-        </Form>
-      </Formik>
-
-      <p className={styles.loginPrompt}>
-        Вже маєте аккаунт?{' '}
-        <Link href="/auth/login" className={styles.loginLink}>
-          Увійти
-        </Link>
-      </p>
     </section>
   );
 }
