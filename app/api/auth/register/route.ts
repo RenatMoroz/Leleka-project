b0c394f7938d08c711b027b6a9696138508678cf
Renat Moroz <bs4123555@gmail.com>
2026-01-13 19:07:56 +0100

    add fix

--- app/api/auth/register/route.ts
+++ app/api/auth/register/route.ts
@@ -1,15 +1,18 @@
 import { NextRequest, NextResponse } from 'next/server';
-import { NextServer } from '@/lib/api/api';
+import { lehlekaApi } from '../../api';
+import { cookies } from 'next/headers';
+import { parse } from 'cookie';
 import { isAxiosError } from 'axios';
+import { logErrorResponse } from '../../_utils/utils';
 
 export async function POST(req: NextRequest) {
   try {
     const body = await req.json();
 
-    const apiRes = await lehlekaApi.post("/auth/register", body);
+    const apiRes = await lehlekaApi.post('/auth/register', body);
 
     const cookieStore = await cookies();
-    const setCookie = apiRes.headers["set-cookie"];
+    const setCookie = apiRes.headers['set-cookie'];
 
     if (setCookie) {
       const cookieArray = Array.isArray(setCookie) ? setCookie : [setCookie];
@@ -19,17 +22,17 @@ export async function POST(req: NextRequest) {
         const options = {
           expires: parsed.Expires ? new Date(parsed.Expires) : undefined,
           path: parsed.Path,
-          maxAge: Number(parsed["Max-Age"]),
+          maxAge: Number(parsed['Max-Age']),
         };
         if (parsed.accessToken)
-          cookieStore.set("accessToken", parsed.accessToken, options);
+          cookieStore.set('accessToken', parsed.accessToken, options);
         if (parsed.refreshToken)
-          cookieStore.set("refreshToken", parsed.refreshToken, options);
+          cookieStore.set('refreshToken', parsed.refreshToken, options);
       }
       return NextResponse.json(apiRes.data, { status: apiRes.status });
     }
 
-    return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
+    return NextResponse.json({ error: 'Unauthorized' }, { status: 401 });
   } catch (error) {
     if (isAxiosError(error)) {
       return NextResponse.json(
